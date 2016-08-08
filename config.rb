9b480ff88033fa64dfc0403b92632bb97fd100af
Michael Dusong <michael@regiomag.net>
2016-08-08 16:59:19 +0100

    added dynami flat pages and desciption

--- config.rb
+++ config.rb
@@ -89,3 +89,19 @@ activate :deploy do |deploy|
   # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
+
+# Getting data for flats pages
+#
+# members = []
+# data.flats[owner].each do |member|
+#   members << member
+# end
+
+# members.each do |member|
+#   proxy "/flat/#{member}.html", "/flat/show.html", :locals => { :owner => member }
+# end
+
+
+["anne", "seb", "romain"].each do |name|
+  proxy "/flat/#{name}.html", "/flat/show.html", :locals => { :owner => name }, :ignore => true
+end
